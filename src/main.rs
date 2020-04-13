f255f00bfa65733ea768e78f326201d3da2e7ac7
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2020-04-13 09:14:39 -0600

    Add file encryption/decryption and release workflow (#14)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use ironoxide::{
     config::IronOxideConfig,
+    document::{DocumentEncryptOpts, DocumentOps, ExplicitGrant, UserOrGroup},
     group::{GroupCreateOpts, GroupId, GroupOps},
     user::{UserCreateResult, UserId, UserOps},
     DeviceContext, IronOxide, IronOxideErr,
 };
+use itertools::EitherOrBoth;
 use serde::Deserialize;
 use std::{
     convert::TryFrom,
+    ffi::OsString,
     fmt,
     fs::File,
     path::PathBuf,
@@ -98,6 +101,34 @@ enum CommandLineArgs {
         /// Path to the calling user's device context
         device_path: PathBuf,
     },
+    /// Encrypt a file to given users and groups. The calling user will automatically be added to the grants list.
+    FileEncrypt {
+        /// Path to the file to encrypt
+        filename: PathBuf,
+        /// Path to the calling user's device context
+        #[structopt(short, long = "device")]
+        device_path: PathBuf,
+        /// Users who will be granted access to the document
+        #[structopt(short, long, parse(try_from_str = parse_user_id))]
+        users: Vec<UserId>,
+        /// Groups that will be granted access to the document
+        #[structopt(short, long, parse(try_from_str = parse_group_id))]
+        groups: Vec<GroupId>,
+        /// Encrypted output file to write [default: "<filename>.iron"]
+        #[structopt(short, long)]
+        output: Option<PathBuf>,
+    },
+    /// Decrypt a file
+    FileDecrypt {
+        /// Path to the file to decrypt
+        filename: PathBuf,
+        /// Path to the calling user's device context
+        #[structopt(short, long = "device")]
+        device_path: PathBuf,
+        /// Decrypted output file to write [default: "<filename> - .iron"]
+        #[structopt(short, long)]
+        output: Option<PathBuf>,
+    },
 }
 
 fn parse_user_id(user_id_string: &str) -> Result<UserId> {
@@ -137,8 +168,8 @@ async fn main() -> Result<()> {
             let config_file = File::open(config_file_path)?;
             let config: InputConfig = serde_json::from_reader(config_file)?;
 
-            let output_file_path =
-                maybe_output_file_path.unwrap_or(PathBuf::from(format!("{}.json", user_id.id())));
+            let output_file_path = maybe_output_file_path
+                .unwrap_or_else(|| PathBuf::from(format!("{}.json", user_id.id())));
             if output_file_path.display().to_string().contains('/') {
                 Err(InitAppErr(
                     "Output file path cannot contain any of the following characters: /"
@@ -171,7 +202,6 @@ async fn main() -> Result<()> {
             let group_futures = group_ids
                 .iter()
                 .map(|group_id| create_group(&sdk, group_id));
-
             futures::future::try_join_all(group_futures).await?;
         }
         CommandLineArgs::GroupAddAdmins {
@@ -240,8 +270,135 @@ async fn main() -> Result<()> {
                 .collect::<Vec<_>>();
             println!("Groups found: {:?}", group_ids)
         }
+        CommandLineArgs::FileEncrypt {
+            filename: infile,
+            device_path,
+            users,
+            groups,
+            output: maybe_output,
+        } => {
+            let file = std::fs::read(&infile)?;
+            println!("Read in file \"{}\"", infile.display());
+            let output = validate_encrypt_output_path(maybe_output, &infile)?;
+            let users_or_groups = collect_users_and_groups(&users, &groups);
+            let sdk = initialize_sdk_from_file(&device_path).await?;
+            encrypt_bytes_to_file(sdk, &file, &users_or_groups, &output).await?;
+        }
+        CommandLineArgs::FileDecrypt {
+            filename: infile,
+            device_path,
+            output: maybe_output,
+        } => {
+            let file = std::fs::read(&infile)?;
+            println!("Read in file \"{}\"", infile.display());
+            let output = validate_decrypt_output_path(maybe_output, infile)?;
+            let sdk = initialize_sdk_from_file(&device_path).await?;
+            let decrypt_result = sdk.document_decrypt(&file).await?;
+            std::fs::write(&output, decrypt_result.decrypted_data())?;
+            println!("Output decrypted file to \"{}\"", output.display());
+        }
     }
     Ok(())
+}
+
+/// Encrypt the provided file to the `users_or_groups`. The file will also be granted to the calling user.
+/// The bytes of the encrypted file will be written to `output_path`.
+async fn encrypt_bytes_to_file(
+    sdk: IronOxide,
+    file: &[u8],
+    users_or_groups: &[UserOrGroup],
+    output_path: &PathBuf,
+) -> Result<()> {
+    let grants = ExplicitGrant::new(true, users_or_groups);
+    let opts = DocumentEncryptOpts::new(None, None, EitherOrBoth::Left(grants));
+    let encrypt_result = sdk.document_encrypt(file, &opts).await?;
+    let successes = encrypt_result
+        .grants()
+        .iter()
+        .map(|u_or_g| match u_or_g {
+            UserOrGroup::User { id } => format!("User: {}", id.id()),
+            UserOrGroup::Group { id } => format!("Group: {}", id.id()),
+        })
+        .collect::<Vec<_>>();
+    let failures = encrypt_result
+        .access_errs()
+        .iter()
+        .map(|edit_err| match &edit_err.user_or_group {
+            UserOrGroup::User { id } => format!("User: {}, Error: {}", id.id(), edit_err.err),
+            UserOrGroup::Group { id } => format!("Group: {}, Error: {}", id.id(), edit_err.err),
+        })
+        .collect::<Vec<_>>();
+    println!("Successfully encrypted file to: {:#?}", successes);
+    println!("Failed to encrypt file to: {:#?}", failures);
+    std::fs::write(&output_path, encrypt_result.encrypted_data())?;
+    println!("Output encrypted file to \"{}\"", output_path.display());
+    Ok(())
+}
+
+/// Collect a vector of `UserId` and a vector of `GroupId` into a vector of `UserOrGroup`.
+fn collect_users_and_groups(user_ids: &[UserId], group_ids: &[GroupId]) -> Vec<UserOrGroup> {
+    let mut users_or_groups = user_ids
+        .iter()
+        .map(|user| user.into())
+        .collect::<Vec<UserOrGroup>>();
+    let mut groups = group_ids
+        .iter()
+        .map(|group| group.into())
+        .collect::<Vec<UserOrGroup>>();
+    users_or_groups.append(&mut groups);
+    users_or_groups
+}
+
+/// Validate that the output path provided by the user can be used for decryption. If no path is provided,
+/// will try to infer an appropriate path for output, otherwise will return an Err.
+fn validate_decrypt_output_path(maybe_output: Option<PathBuf>, infile: PathBuf) -> Result<PathBuf> {
+    match maybe_output {
+        Some(output) => Ok(output),
+        None => {
+            let extension = infile.extension().ok_or_else(|| {
+                InitAppErr("No output file given, and unable to infer.".to_string())
+            })?;
+            if extension.to_os_string() == OsString::from("iron") {
+                let mut output_path = infile;
+                output_path.set_extension("");
+                Ok(output_path)
+            } else {
+                // Unknown extension on infile
+                Err(InitAppErr(
+                    "No output file given, and unable to infer.".to_string(),
+                ))
+            }
+        }
+    }
+}
+
+/// Validate that the output path provided by the user can be used for encryption. If no path is provided,
+/// will append ".iron" to the input filename. Returns an Err if the input file ends with "..".
+fn validate_encrypt_output_path(
+    maybe_output: Option<PathBuf>,
+    infile: &PathBuf,
+) -> Result<PathBuf> {
+    let output = match maybe_output {
+        // User specified an output path.
+        Some(desired) => {
+            // User specified a directory for output
+            if desired.is_dir() {
+                let mut filename = infile
+                    .file_name()
+                    .ok_or_else(|| InitAppErr("Invalid input file".to_string()))?
+                    .to_os_string();
+                filename.push(".iron");
+                let mut desired_dir = desired;
+                desired_dir.push(filename);
+                desired_dir
+            } else {
+                desired
+            }
+        }
+        // User didn't specify an output path. Add ".iron" to the input path.
+        None => PathBuf::from(infile.display().to_string() + ".iron"),
+    };
+    Ok(output)
 }
 
 enum GroupModificationFunction {
@@ -253,22 +410,22 @@ enum GroupModificationFunction {
 
 async fn modify_group(
     group_id: &GroupId,
-    user_ids: &Vec<UserId>,
+    user_ids: &[UserId],
     device_path: &PathBuf,
     modification_function: GroupModificationFunction,
 ) -> Result<()> {
     let sdk = initialize_sdk_from_file(device_path).await?;
     let modify_result = match modification_function {
-        GroupModificationFunction::AddAdmins => sdk.group_add_admins(&group_id, &user_ids),
-        GroupModificationFunction::RemoveAdmins => sdk.group_remove_admins(&group_id, &user_ids),
-        GroupModificationFunction::AddMembers => sdk.group_add_members(&group_id, &user_ids),
-        GroupModificationFunction::RemoveMembers => sdk.group_remove_members(&group_id, &user_ids),
+        GroupModificationFunction::AddAdmins => sdk.group_add_admins(group_id, user_ids),
+        GroupModificationFunction::RemoveAdmins => sdk.group_remove_admins(group_id, user_ids),
+        GroupModificationFunction::AddMembers => sdk.group_add_members(group_id, user_ids),
+        GroupModificationFunction::RemoveMembers => sdk.group_remove_members(group_id, user_ids),
     }
     .await?;
     let successes = modify_result
         .succeeded()
         .iter()
-        .map(|user| user.id())
+        .map(UserId::id)
         .collect::<Vec<_>>();
     let failures = modify_result
         .failed()
@@ -448,4 +605,115 @@ fn gen_jwt(user: &UserCreate) -> Jwt {
     .expect("You don't appear to have the proper identity assertion key to sign the JWT.");
 
     Jwt(jwt)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn validate_encrypt_with_no_output_path() -> Result<()> {
+        let maybe_output = None;
+        let infile = PathBuf::from("test");
+        let output = validate_encrypt_output_path(maybe_output, &infile)?;
+        let expected_output = PathBuf::from("test.iron");
+        assert_eq!(output, expected_output);
+        Ok(())
+    }
+
+    #[test]
+    fn validate_encrypt_with_directory_output_path() -> Result<()> {
+        let maybe_output = Some(PathBuf::from("target"));
+        let infile = PathBuf::from("test");
+        let output = validate_encrypt_output_path(maybe_output, &infile)?;
+        let expected_output = PathBuf::from("target/test.iron");
+        assert_eq!(output, expected_output);
+        Ok(())
+    }
+
+    #[test]
+    fn validate_encrypt_with_output_path() -> Result<()> {
+        let maybe_output = Some(PathBuf::from("test2.iron"));
+        let infile = PathBuf::from("test");
+        let output = validate_encrypt_output_path(maybe_output.clone(), &infile)?;
+        assert_eq!(Some(output), maybe_output);
+        Ok(())
+    }
+
+    #[test]
+    fn validate_encrypt_with_longer_output_path() -> Result<()> {
+        let maybe_output = Some(PathBuf::from("target/debug/test2.iron"));
+        let infile = PathBuf::from("test");
+        let output = validate_encrypt_output_path(maybe_output.clone(), &infile)?;
+        assert_eq!(Some(output), maybe_output);
+        Ok(())
+    }
+
+    #[test]
+    fn validate_decrypt_with_no_extension() -> Result<()> {
+        let maybe_output = None;
+        let infile = PathBuf::from("test");
+        let output = validate_decrypt_output_path(maybe_output, infile);
+        assert!(output.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn validate_decrypt_with_unknown_extension() -> Result<()> {
+        let maybe_output = None;
+        let infile = PathBuf::from("test.whoknows");
+        let output = validate_decrypt_output_path(maybe_output, infile);
+        assert!(output.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn validate_decrypt_with_iron_extension() -> Result<()> {
+        let maybe_output = None;
+        let infile = PathBuf::from("test.iron");
+        let output = validate_decrypt_output_path(maybe_output, infile)?;
+        let expected_output = PathBuf::from("test");
+        assert_eq!(output, expected_output);
+        Ok(())
+    }
+
+    #[test]
+    fn validate_decrypt_with_multiple_extensions() -> Result<()> {
+        let maybe_output = None;
+        let infile = PathBuf::from("test.random.extension.iron");
+        let output = validate_decrypt_output_path(maybe_output, infile)?;
+        let expected_output = PathBuf::from("test.random.extension");
+        assert_eq!(output, expected_output);
+        Ok(())
+    }
+
+    #[test]
+    fn collect_one_user_and_group() {
+        let user = UserId::unsafe_from_string("a".to_string());
+        let group = GroupId::unsafe_from_string("b".to_string());
+        let user_ids = vec![user.clone()];
+        let group_ids = vec![group.clone()];
+        let users_or_groups = collect_users_and_groups(&user_ids, &group_ids);
+        let expected = vec![
+            UserOrGroup::User { id: user },
+            UserOrGroup::Group { id: group },
+        ];
+        assert_eq!(users_or_groups, expected);
+    }
+
+    #[test]
+    fn collect_one_user_and_no_groups() {
+        let user = UserId::unsafe_from_string("a".to_string());
+        let user_ids = vec![user.clone()];
+        let users_or_groups = collect_users_and_groups(&user_ids, &[]);
+        let expected = vec![UserOrGroup::User { id: user }];
+        assert_eq!(users_or_groups, expected);
+    }
+
+    #[test]
+    fn collect_no_users_and_groups() {
+        let users_or_groups = collect_users_and_groups(&[], &[]);
+        let expected = vec![];
+        assert_eq!(users_or_groups, expected);
+    }
 }
