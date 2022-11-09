d26abbc29ef1c965b5785f4f62d63bb77c144f99
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2022-11-08 17:29:35 -0700

    Initial attempt

--- flake.nix
+++ flake.nix
@@ -1,48 +1,43 @@
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
   };
 
-  outputs = inputs: with inputs; # pass through all inputs and bring them into scope
-
-    # Build the output set for each default system and map system sets into
-    # attributes, resulting in paths such as:
-    # nix build .#packages.x86_64-linux.<name>
-    flake-utils.lib.eachDefaultSystem (system:
-
-      # let-in expressions, very similar to Rust's let bindings.  These names
-      # are used to express the output but not themselves paths in the output.
-      let
-
-        # create nixpkgs that contains rustBuilder from cargo2nix overlay
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cargo2nix.overlays.default ];
-        };
-
-        # create the workspace & dependencies package set
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.61.0";
-          packageFun = import ./Cargo.nix;
-          # packageOverrides = pkgs: pkgs.rustBuilder.overrides.all; # Implied, if not specified
-        };
-
-      in rec {
-        # this is the output (recursive) set (expressed for each system)
-
-        # the packages in `nix build .#packages.<system>.<name>`
-        packages = {
-          # nix build .#ironoxide-cli
-          # nix build .#packages.x86_64-linux.ironoxide-cli
-          ironoxide-cli = (rustPkgs.workspace.ironoxide-cli {}).bin;
-          # nix build
-          default = packages.ironoxide-cli;
-          ci = pkgs.rustBuilder.runTests rustPkgs.workspace.ironoxide-cli {
-            /* Add `depsBuildBuild` test-only deps here, if any. */
+  outputs = inputs:
+    with inputs; # pass through all inputs and bring them into scope
+    nci.lib.makeOutputs {
+      root = ./.;
+      config = common: {
+        cachix.name = "ironoxide-cli";
+        # public key
+        cachix.key =
+          "ironoxide-cli.cachix.org-1:ergKeYqlV1ZxVQa5qdsMpxtUAi1+h+oxODTgbIWGIWg=";
+        cCompiler.package = with common.pkgs;
+          if stdenv.isLinux then gcc else clang;
+        outputs = {
+          defaults = {
+            app = "ironoxide-cli";
+            package = "ironoxide-cli";
           };
         };
-      }
-    );
+      };
+      pkgConfig = common: {
+        ironoxide-cli = {
+          overrides.fix-build.overrideAttrs = prev: {
+            buildInputs = nci.lib.nci-lib.addBuildInputs prev [ ]
+              ++ nixpkgs.lib.optionals common.pkgs.stdenv.isDarwin
+              [ common.pkgs.darwin.apple_sdk.frameworks.Security ];
+          };
+        };
+      };
+    };
 }
