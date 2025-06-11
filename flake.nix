f991dcfc1689cd0e115bd0b3821c03491fb78246
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2025-06-11 14:27:34 -0700

    Bump to 0.2.0 (#305)

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         packages = {
           ironoxide-cli = pkgs.rustPlatform.buildRustPackage {
             pname = "ironoxide-cli";
-            version = "0.1.0";
+            version = "0.2.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
             nativeBuildInputs = [ pkgs.pkg-config ];
